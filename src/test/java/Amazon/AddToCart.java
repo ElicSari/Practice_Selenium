afb3e79593c355f64a76000d1805510ca50b5f1b
elkhan <elkhanelay@gmail.com>
2022-05-29 21:09:46 -0400

    find amazon,searchBox , find Alexa

---
+++ src/test/java/Amazon/AddToCart.java
@@ -0,0 +1,21 @@
+package Amazon;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AddToCart {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://www.amazon.com");
+        driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys("Alexa", Keys.ENTER);
+
+        Thread.sleep(2000);
+
+    }
+}
