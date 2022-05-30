ba5b316db29df4375974aefadeed4811f9e05959
elkhan <elkhanelay@gmail.com>
2022-05-29 20:09:36 -0400

    Initial commit

---
+++ src/test/java/GoogleEmail/Email.java
@@ -0,0 +1,20 @@
+package GoogleEmail;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Email {
+    public static void main(String[] args) {
+
+      WebDriverManager.chromedriver().setup();
+      WebDriver driver = new ChromeDriver();
+      driver.manage().window().maximize();
+
+      
+
+
+
+      //  driver.quit();
+    }
+}
