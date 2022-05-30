3b616ea5b81204629670d09c05655dfa1aca9419
elkhan <elkhanelay@gmail.com>
2022-05-29 20:37:45 -0400

    Find google,find gmail,find sign in

--- src/test/java/GoogleEmail/Email.java
+++ src/test/java/GoogleEmail/Email.java
@@ -1,17 +1,22 @@
 package GoogleEmail;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Email {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
       WebDriverManager.chromedriver().setup();
       WebDriver driver = new ChromeDriver();
       driver.manage().window().maximize();
 
-      
+     driver.get("https://www.google.com");
+     driver.findElement(By.className("gb_d")).click();
+
+     Thread.sleep(3000);
+     driver.findElement(By.xpath("//a[@data-action='sign in']")).click();
 
 
 
